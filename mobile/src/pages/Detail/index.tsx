c957ea1b9dd83c8f3bc323d56a9507b871b95da6
Anthony D <anthonydantas15@gmail.com>
2020-06-14 14:06:48 -0300

    App terminado a tela de maps

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  Constants from 'expo-constants'
+import { Feather as Icon } from '@expo/vector-icons'
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+
+const Detail = () =>{
+    return (
+    <View />
+    )
+}
+
+export default Detail
